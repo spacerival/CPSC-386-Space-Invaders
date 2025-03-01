fefb02be7318c838ee672cf2cfd96fad08cdb549
spacerival <chantal.le21003@gmail.com>
2025-02-28 17:14:44 -0800

    Update settings.py
    added helper functions to play menu theme and game music

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+import pygame as pg
 from colors import DARK_GREY, RED
 
 class Settings:
@@ -37,8 +38,23 @@ class Settings:
         self.alien_points = int(self.alien_points * self.score_scale)
 
 
+    def play_menu_theme(self):
+        pg.mixer.music.unload()
+        pg.mixer.music.load("Space Invader Project Menu Theme.mp3")
+        pg.mixer.music.set_volume(0.3)
+        pg.mixer.music.play(-1, 0)
+    
+    
+    def play_game_music(self):
+        pg.mixer.music.unload()
+        pg.mixer.music.load("Space Invader Project Game Music.mp3")
+        pg.mixer.music.set_volume(0.75)
+        pg.mixer.music.play(-1, 0)
+
+
 def main():
     print('\n*** message from settings.py --- run from alien_invasions.py\n')
 
 if __name__ == "__main__":
     main()
+

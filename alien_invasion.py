1b5c605bca4089c7cdd0d2f41bfc9537ab1b7d8a
spacerival <chantal.le21003@gmail.com>
2025-03-01 18:35:52 -0800

    Update alien_invasion.py
    High score button and back button works. Can change between start menu and high score menu. Will also update high score list every time the game is over

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,7 +9,6 @@ from game_stats import GameStats
 from button import Button
 from scoreboard import Scoreboard
 from event import Event
-
 from menu import Menu
 from pathlib import Path
 
@@ -27,7 +26,7 @@ class AlienInvasion:
         self.screen = pg.display.set_mode(self.settings.w_h)
         self.stats = GameStats(self)
         self.sb = Scoreboard(self)
-        self.menu = Menu()
+        self.menu = Menu(self)
         self.ship = Ship(ai_game=self)
         self.fleet = Fleet(ai_game=self)
         self.ship.set_fleet(self.fleet)
@@ -39,30 +38,31 @@ class AlienInvasion:
         # Start Alien Invasion in an inactive state.
         self.game_active = False
         self.first = True
+        self.menu_toggle = True
 
         self.play_button = Button(self, "Play")
         self.high_score_button = Button(self, "High Scores", BLUE, 75)
+        self.back_button = Button(self, "Back",BLUE, 350)
         self.event = Event(self)
         pg.mixer.music.load("Space Invader Project Menu Theme.mp3")
         pg.mixer.music.set_volume(0.3)
-        
+
 
     def game_over(self):
         self.restart_game()
-        print(f"{self.stats.high_score}\n")
-        self.path.write_text(f"{self.stats.high_score}")
+        print(f"{self.stats.high_score}\n")     # Testing purposes
         print("Game over!")
         self.game_active = False
+        self.menu_toggle = True
         pg.mouse.set_visible(True)
         self.settings.play_menu_theme()
-        #sys.exit()
+    
 
     def reset_game(self):
         self.settings.play_game_music()
         self.stats.reset_stats()
         self.sb.prep_score_level_ships()
         self.game_active = True
-
         self.ship.reset_ship()
         self.fleet.reset_fleet()
         pg.mouse.set_visible(False)
@@ -71,7 +71,9 @@ class AlienInvasion:
         self.game_active = False
         self.first = True
         self.play_button.reset_message("Play again? (q for quit)")
+        self.menu.update_score_list(self.stats.score)
         self.reset_game()
+
 
     def run_game(self):
         self.finished = False
@@ -88,9 +90,14 @@ class AlienInvasion:
                 self.sb.show_score()
 
             if not self.game_active:
-                self.menu.display_menu()
-                self.play_button.draw_button()
-                self.high_score_button.draw_button()
+                if self.menu_toggle == True:
+                    self.menu.display_menu()
+                    self.play_button.draw_button()
+                    self.high_score_button.draw_button()
+                else:
+                    self.menu.display_high_scores()
+                    self.back_button.draw_button()
+                
             pg.display.flip()
 
             self.clock.tick(60)

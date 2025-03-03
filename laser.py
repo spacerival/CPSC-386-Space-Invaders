a64fceb61d237a9880294602c1d82c3e4e254f7d
spacerival <chantal.le21003@gmail.com>
2025-03-02 21:52:53 -0800

    Update laser.py
    laser class can now be used for both the ships and aliens

--- laser.py
+++ laser.py
@@ -6,7 +6,7 @@ class Laser(Sprite):
     @staticmethod
     def random_color(): 
         return (randint(0, 255), randint(0, 255), randint(0, 255))
-    def __init__(self, ai_game):
+    def __init__(self, ai_game, isAlien=False, midtop_rect=(0,0)):
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
@@ -14,11 +14,14 @@ class Laser(Sprite):
         self.color = Laser.random_color()
         self.rect = pg.Rect(0, 0, self.settings.laser_width,
                                 self.settings.laser_height)
-        self.rect.midtop = ai_game.ship.rect.midtop
+        if isAlien == True:
+            self.rect.midtop = midtop_rect
+        else:
+            self.rect.midtop = ai_game.ship.rect.midtop
         self.y = float(self.rect.y)
 
-    def update(self):
-        self.y -= self.settings.laser_speed
+    def update(self, direction=1):
+        self.y -= self.settings.laser_speed * direction
         self.rect.y = self.y
 
     def draw(self):

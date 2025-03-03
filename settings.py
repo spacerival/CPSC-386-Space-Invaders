99aa2133811feae45490e727c03c51eaab3d843e
spacerival <chantal.le21003@gmail.com>
2025-03-02 21:50:36 -0800

    Update settings.py
    uses updated music

--- settings.py
+++ settings.py
@@ -48,7 +48,7 @@ class Settings:
     
     def play_game_music(self):
         pg.mixer.music.unload()
-        pg.mixer.music.load("Space Invader Project Game Music.mp3")
+        pg.mixer.music.load("Space Invader Project Game Music new.mp3")
         pg.mixer.music.set_volume(0.75)
         pg.mixer.music.play(-1, 0)
 

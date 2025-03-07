52834212475419b7a7b48ebb37ac5127507fa9ae
spacerival <chantal.le21003@gmail.com>
2025-03-07 03:02:20 -0800

    Update fleet.py
    now earn different points depending on alien type. 
    shifted fleet down a little to make space for UFO.
    UFO spawns at random times and only once per level.
    player will get 500 points for defeating a UFO
    ship will be destroyed if it hits the UFO

--- fleet.py
+++ fleet.py
@@ -13,6 +13,7 @@ class Fleet(Sprite):
         self.screen = ai_game.screen
         self.ship = ai_game.ship
         self.aliens = pg.sprite.Group()
+        self.ufo = pg.sprite.GroupSingle()
         self.lasers = pg.sprite.Group()
         self.settings = ai_game.settings
         self.stats = ai_game.stats
@@ -20,18 +21,21 @@ class Fleet(Sprite):
         self.v = Vector(self.settings.alien_speed, 0)
         # alien = Alien(ai_game=ai_game)
         # self.aliens.add(alien)
-        self.spacing = 1.4
+        self.spacing = 1.2
         self.create_fleet()
+        self.ufo_on = False
+        self.ufo_level = 0
         # self.create_row()
 
     def reset_fleet(self):
         self.aliens.empty()
         self.create_fleet()
 
+
     def create_fleet(self):
         alien = Alien(ai_game=self.ai_game, v=self.v)
         alien_height = alien.rect.height
-        current_y = alien_height
+        current_y = alien_height + 65
         type = 2
         while current_y < (self.settings.scr_height - self.spacing * 6 * alien_height):
             if type == -1:
@@ -40,12 +44,13 @@ class Fleet(Sprite):
             current_y += self.spacing * alien_height
             type -= 1
         
-    def create_row(self, y, type):
-        alien = Alien(ai_game=self.ai_game, v=self.v, type=type)
+
+    def create_row(self, y, alien_type):
+        alien = Alien(ai_game=self.ai_game, v=self.v, type=alien_type)
         alien_width = alien.rect.width
         current_x = alien_width 
         while current_x < (self.settings.scr_width - self.spacing * alien_width):
-             new_alien = Alien(self, v=self.v, type=type)
+             new_alien = Alien(self, v=self.v, type=alien_type)
              new_alien.rect.y = y
              new_alien.y = y
              new_alien.x = current_x
@@ -53,11 +58,22 @@ class Fleet(Sprite):
              self.aliens.add(new_alien)
              current_x += self.spacing * alien_width
 
+    
+    def spawn_ufo(self):
+        alien = Alien(self.ai_game, self.v, 2)
+        print(alien.check_pts)
+        self.ufo.add(alien)
+        print("UFO SPAWNED")
+        self.ufo_on = True
+        self.ufo_level += 1
+
+
     def check_edges(self):
         for alien in self.aliens:
             if alien.check_edges(): 
                 return True 
         return False
+    
     
     def check_bottom(self):
         for alien in self.aliens:
@@ -72,14 +88,36 @@ class Fleet(Sprite):
         self.lasers.add(laser) 
     
 
+    def update_laser(self):
+        count = 0
+        while count < 13:
+            self.lasers.update(-0.1)
+            for laser in self.lasers.copy():
+                if laser.rect.bottom >= self.settings.scr_height:
+                    self.lasers.remove(laser)
+            for laser in self.lasers.sprites():
+                laser.draw()  
+            count += 1  
+
     def update(self): 
-        collisions = pg.sprite.groupcollide(self.ship.lasers, self.aliens, True, True)
+        alien_defeated = pg.sprite.groupcollide(self.ship.lasers, self.aliens, True, True)
+        ufo_defeated = pg.sprite.groupcollide(self.ship.lasers, self.ufo, True, True)
+        ship_alien_hit = pg.sprite.spritecollideany(self.ship, self.aliens)
+        ship_alien_laser_hit =  pg.sprite.spritecollideany(self.ship, self.lasers)
+        ship_ufo_hit = pg.sprite.spritecollideany(self.ship, self.ufo)
         pg.sprite.groupcollide(self.ship.lasers, self.lasers, True, True)
 
-        if collisions:
-            for aliens in collisions.values():
-                self.stats.score += self.settings.alien_points * len(aliens)
+        if alien_defeated:
+            for aliens in alien_defeated.values():
+                for alien in aliens:
+                    pts = alien.check_pts()
+                self.stats.score += pts
                 self.settings.alien_speed *= 1.03
+            self.sb.prep_score()
+            self.sb.check_high_score()
+        elif ufo_defeated:
+            self.stats.score += Alien.alien3.get("points")
+            self.ufo_on = False
             self.sb.prep_score()
             self.sb.check_high_score()
 
@@ -92,13 +130,16 @@ class Fleet(Sprite):
             self.stats.level += 1
             self.sb.prep_level()
             return
-        if pg.sprite.spritecollideany(self.ship, self.aliens) or pg.sprite.spritecollideany(self.ship, self.lasers):
+        
+        if ship_alien_hit or ship_alien_laser_hit or ship_ufo_hit:
             print("Ship hit!")
+            self.settings.alien_speed = self.settings.alien_base_speed
             self.ship.ship_hit()
             self.lasers.empty()
             return
         
         if self.check_bottom():
+            self.settings.alien_speed = self.settings.alien_base_speed
             return 
         
         if self.check_edges():
@@ -106,22 +147,24 @@ class Fleet(Sprite):
             for alien in self.aliens:
                 alien.v.x = self.v.x
                 alien.y += self.settings.fleet_drop_speed
+    
+        
+        if pg.time.get_ticks() % randint(600, 700) == 0 and self.ufo_level != self.stats.level:
+            if self.ufo_on == False:
+                self.spawn_ufo()
+
             
         for alien in self.aliens:
             alien.update()
-            if pg.time.get_ticks() % randint(500, 650) == 0:
-                if randint(1, 25) == 1:                 
+            if pg.time.get_ticks() % randint(500, 675) == 0:
+                if randint(1, 22) == 1:                 
                     self.fire_laser(alien.rect.midtop)
+        for ufo in self.ufo:
+            ufo.update()
+
+        self.update_laser()
         
-        count = 0
-        while count < 13:
-            self.lasers.update(-0.1)
-            for laser in self.lasers.copy():
-                if laser.rect.bottom >= self.settings.scr_height:
-                    self.lasers.remove(laser)
-            for laser in self.lasers.sprites():
-                laser.draw()  
-            count += 1  
+        
 
     def draw(self): pass
         # for alien in self.aliens:

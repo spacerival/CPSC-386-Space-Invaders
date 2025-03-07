5432c0a09486e0058c61290c116d3122654d60e8
spacerival <chantal.le21003@gmail.com>
2025-03-07 02:56:05 -0800

    Update settings.py
    got rid of alien_type points values (uses dictionaries instead from the alien class). gets music from folder instead of directly using the file

--- settings.py
+++ settings.py
@@ -29,10 +29,7 @@ class Settings:
         self.alien_speed = self.alien_base_speed
 
         self.alien_points = 50 # will remove later to account for different alien types/points
-        self.pink_alien_pts = 100
-        self.blue_alien_pts = 200
-        self.green_alien_pts = 300
-        self.ufo_pts = 500
+        
 
     def increase_speed(self):
         """Increase speed settings and alien point values."""
@@ -45,14 +42,14 @@ class Settings:
 
     def play_menu_theme(self):
         pg.mixer.music.unload()
-        pg.mixer.music.load("Space Invader Project Menu Theme.mp3")
+        pg.mixer.music.load("Space Invaders Audio/Space Invader Project Menu Theme.mp3")
         pg.mixer.music.set_volume(0.3)
         pg.mixer.music.play(-1, 0)
     
     
     def play_game_music(self):
         pg.mixer.music.unload()
-        pg.mixer.music.load("Space Invader Project Game Music new.mp3")
+        pg.mixer.music.load("Space Invaders Audio/Space Invader Project Game Music new.mp3")
         pg.mixer.music.set_volume(0.75)
         pg.mixer.music.play(-1, 0)
 

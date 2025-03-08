7daef05c593eb6eb4cba3681802dd3006b2be555
BereniceAce <113072957+BereniceAce@users.noreply.github.com>
2025-03-08 00:04:19 -0800

    Update timer.py
    alien explosions

--- timer.py
+++ timer.py
@@ -2,7 +2,7 @@ import pygame
 from time import time
 
 class Timer:
-    def __init__(self, images, start_index=0, loop_continuously=True, delta=1000):
+    def __init__(self, images, start_index=0, loop_continuously=True, delta=1000, running = True):
         if len(images) == 0: raise ValueError("timer's list of images is empty")
         if start_index > len(images) - 1: raise ValueError("start_index out of bounds")
         self.images = images
@@ -10,11 +10,17 @@ class Timer:
         self.loop_continuously = loop_continuously
         self.index = start_index
         self.latest = pygame.time.get_ticks()
+        self.running = running
+
+
+    def start(self): self.running = True
 
     def finished(self): 
         return not self.loop_continuously and self.index == len(self.images) - 1
 
     def current_image(self):
+        if not self.running: return self.images[self.index]
+
         now = pygame.time.get_ticks()
         if now - self.latest > self.delta and not self.finished(): 
             # print(f'now: {now}, latest: {self.latest}, now - latest: {now - self.latest}, delta: {self.delta}, index: {self.index}')
